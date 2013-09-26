332dcad8465d924a5ee303996f8f423e00dc602d
Greg Karlin <gregory.l.karlin@gmail.com>
2013-09-26 10:37:00 -0400

    change jesus

---
+++ Jesus/PoemData/Poem.m
@@ -0,0 +1,19 @@
+//
+//  Poem.m
+//  PoemData
+//
+//  Created by Greg Karlin on 9/25/13.
+//  Copyright (c) 2013 Greg Karlin. All rights reserved.
+//
+
+#import "Poem.h"
+#import "Category.h"
+
+
+@implementation Poem
+
+@dynamic content;
+@dynamic title;
+@dynamic category;
+
+@end
